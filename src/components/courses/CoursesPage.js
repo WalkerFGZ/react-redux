c2c56118833df785e11eb26ab6157a3c22749f52
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2024-11-08 23:13:04 -0500

    add optimistic delete and await function

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
+import { deleteCourse, loadCourses } from "../../redux/actions/courseActions";
 import { useDispatch, useSelector } from "react-redux";
 
 import CourseList from "./CourseList";
 import Spinner from "../common/Spinner";
 import { loadAuthors } from "../../redux/actions/authorActions";
-import { loadCourses } from "../../redux/actions/courseActions";
+import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
 export default function CoursesPage() {
@@ -35,13 +36,31 @@ export default function CoursesPage() {
           };
         });
 
+  const handleDeleteCourse = async course => {
+    toast.success("Course deleted");
+    try {
+      await dispatch(deleteCourse(course));
+    } catch (error) {
+      toast.error("Delete failed. " + error.message, {
+        autoClose: false
+      });
+    }
+  };
+
   return (
     <>
       <h2>Courses</h2>
       <button className="btn btn-primary" onClick={() => navigate("/course")}>
         Add Course
       </button>
-      {loading ? <Spinner /> : <CourseList courses={coursesWithAuthors} />}
+      {loading ? (
+        <Spinner />
+      ) : (
+        <CourseList
+          onDeleteClick={handleDeleteCourse}
+          courses={coursesWithAuthors}
+        />
+      )}
     </>
   );
 }
